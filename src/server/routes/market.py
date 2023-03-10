656137f37171e0fc45ac563823eede4aac65440d
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2023-03-10 20:04:46 +0100

    added market info endpoints

--- src/server/routes/market.py
+++ src/server/routes/market.py
@@ -6,6 +6,33 @@ from client import get_ccxt_client
 router = APIRouter()
 
 
+@router.get("/info/")
+async def list_market(x_connection_id: str = Header()):
+    try:
+        client = get_ccxt_client(x_connection_id)
+        return client.load_markets()
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.get("/info/{symbol}/")
+async def retrieve_market(symbol: str, x_connection_id: str = Header()):
+    try:
+        markets = await list_market(x_connection_id=x_connection_id)
+        return markets[symbol]
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.post("/info/sync/")
+async def sync_markets(x_connection_id: str = Header()):
+    try:
+        client = get_ccxt_client(x_connection_id)
+        return client.load_markets(reload=True)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
 @router.get("/ticker/")
 async def list_ticker(x_connection_id: str = Header()):
     try:
